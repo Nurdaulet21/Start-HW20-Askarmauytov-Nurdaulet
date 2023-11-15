35ccac5eab791d1b9ca659d76a969eafa7cbb91d
Nurdaulet Askarmauytov <nurrdau@gmail.com>
2023-11-15 20:52:55 +0500

    Initial Commit

---
+++ hw20/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw20
+//
+//  Created by Нурдаулет on 15.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
